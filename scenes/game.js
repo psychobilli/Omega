6f59953c1591f80c1c741ea378149d676d1552ad
psychobilli <richardsw1123@gmail.com>
2019-01-29 22:37:54 -0500

    Added an exit button.

--- scenes/game.js
+++ scenes/game.js
@@ -13,6 +13,7 @@ class gameScene extends Phaser.Scene {
         this.load.image('forest','assets/images/forest.png');
         this._player.preload(this);
         this.load.image('wall',null);
+        this.load.image('exit','assets/images/Exit.png');
     }
 
     create()
@@ -27,11 +28,19 @@ class gameScene extends Phaser.Scene {
         this._cursors = this.input.keyboard.createCursorKeys();
 
         this.physics.add.collider(this._player.sprite, this._walls);
+
+        const exit = this.add.image(1000, 100, 'exit');
+        exit.setInteractive();
+        exit.on('pointerup', this.exitGame, this);
     }
 
     update()
     {
         this._player.update(this._cursors);
+    }
+
+    exitGame() {
+        this.scene.start('titleScene');
     }
 
 }
