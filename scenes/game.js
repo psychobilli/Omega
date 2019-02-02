be573b9c49da509667ceffe49eb79577c619eaf1
psychobilli <richardsw1123@gmail.com>
2019-02-01 23:47:09 -0500

    Added a rock to the game.

--- scenes/game.js
+++ scenes/game.js
@@ -1,24 +1,30 @@
 class gameScene extends Phaser.Scene {
 
     constructor() {
+        console.log('gameScene constructor');
         super({ key: 'gameScene', active: false });
         this.player;
+        this.rock;
         this.cursors;
         this.walls;
     }
 
     preload()
     {
+        console.log('gameScene Preload');
         this._player = new Player();
         this.load.image('forest','assets/images/forest.png');
         this._player.preload(this);
         this.load.image('wall',null);
+        this.load.image('rock','assets/images/rock.png');
         this.load.image('exit','assets/images/Exit.png');
     }
 
     create()
     {
+        console.log('gameScene Create');
         this.add.image(600,400,'forest');
+        this._rock = this.physics.add.image(600,200,'rock');
         this._walls = this.physics.add.staticGroup();
         this._walls.create(300,400, 'wall').setScale(0,50).refreshBody();
         this._walls.create(900,400, 'wall').setScale(0,50).refreshBody();
@@ -27,8 +33,10 @@ class gameScene extends Phaser.Scene {
 
         this._cursors = this.input.keyboard.createCursorKeys();
 
+        this.physics.add.collider(this._player.sprite, this._rock);
+        this.physics.add.collider(this._rock, this._walls);
         this.physics.add.collider(this._player.sprite, this._walls);
-
+        
         const exit = this.add.image(1000, 100, 'exit');
         exit.setInteractive();
         exit.on('pointerup', this.exitGame, this);
@@ -36,6 +44,7 @@ class gameScene extends Phaser.Scene {
 
     update()
     {
+        console.log('gameScene Update');
         this._player.update(this._cursors);
     }
 
