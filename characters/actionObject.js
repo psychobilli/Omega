d0023a8174600809ae14f53d0a7c16bdea53ab82
psychobilli <richardsw1123@gmail.com>
2019-02-10 23:08:21 -0500

    Corrected a bug which caused the rock to slide.

--- characters/actionObject.js
+++ characters/actionObject.js
@@ -5,6 +5,7 @@ class ActionObject {
         this.image;
         this.physicsImage;
         this.cursors;
+        this.lastCursor;
     }
 
     preload(main, imagePath) {
@@ -17,8 +18,8 @@ class ActionObject {
     }
 
     addPush(playerSprite, cursors) {
-        this._main.physics.add.collider(playerSprite, this._physicsImage, this.push, null, this);
-        this._physicsImage.setDrag(100,100);
+        this._main.physics.add.collider(playerSprite, this._physicsImage, this.push, this.detect, this);
+        this._physicsImage.setDrag(1000,1000);
         this._cursors = cursors;
     }
 
@@ -28,31 +29,43 @@ class ActionObject {
 
     push() {
         console.log('collision detected');
-        if (this._cursors.left.isDown)
+        if (this._cursors.left.isDown && (this._lastCursor == 0 || this._lastCursor == 1))
         {
+            this._lastCursor = 1;
             this._physicsImage.setVelocityX(-160);
             this._physicsImage.setVelocityY(0);
         }
-        else if (this._cursors.right.isDown)
+        else if (this._cursors.right.isDown && (this._lastCursor == 0 || this._lastCursor == 2))
         {
+            this._lastCursor = 2;
             this._physicsImage.setVelocityX(160);
             this._physicsImage.setVelocityY(0);
         }
-        else if (this._cursors.up.isDown)
+        else if (this._cursors.up.isDown && (this._lastCursor == 0 || this._lastCursor == 3))
         {
+            this._lastCursor = 3;
             this._physicsImage.setVelocityX(0);
             this._physicsImage.setVelocityY(-160);
         }
-        else if (this._cursors.down.isDown)
+        else if (this._cursors.down.isDown && (this._lastCursor == 0 || this._lastCursor == 4))
         {
+            this._lastCursor = 4;
             this._physicsImage.setVelocityX(0);
             this._physicsImage.setVelocityY(160);
         } 
         else
         {
+            this._lastCursor = 0;
             this._physicsImage.setVelocityX(0);
             this._physicsImage.setVelocityY(0);
         }
+    }
+
+    detect() {
+        // can be used for additional checks before performing an interaction.
+        // return false to block the action.
+        // can't think of a use case for this.
+        return true;
     }
 
     get image() {
